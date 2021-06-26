251548e9c989acd76a5c6bd233f07da834751c1a
swillrich5 <swillrich@gmail.com>
2021-06-25 19:20:06 -0500

    mongo db set up, routes working

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('password', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcrypt.hashSync('password', 10),
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        password: bcrypt.hashSync('password', 10),
+        isAdmin: true
+    },
+]
+
+export default users;
