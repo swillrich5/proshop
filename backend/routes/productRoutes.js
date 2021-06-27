36713a90ae87b81d8ab626c73723acc93569caf9
swillrich5 <swillrich@gmail.com>
2021-06-27 12:28:06 -0500

    routes working, problem with error handling setup

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,20 +10,24 @@ import Product from '../models/productModel.js';
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
-}))
+}));
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public 
 router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
+    const product = await Product.findById(req.params.id)
+    console.log('product = ');
+    console.log(product);
     if (product) {
+        console.log(product);
         res.json(product);
     } else {
-        res.status(404).json({ message: 'Product not found' });
+        res.status(404);
+        console.log(res.status);
+        throw new Error('Product not found');
     }
-}))
+}));
 
 export default router;
 
